dcaa50c7d3e329ab2a0cb25b5d53a5397c12faed
newcarvalho <new.figueiredo@gmail.com>
2020-06-04 22:44:28 -0300

    dice gerator numero de dados e lados

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,19 @@
 import random
 
 def main():
-    numero_dados = 2
+    numero_dados = int(input("digite a quantidade de dados"))
+    numero_de_lados = int(input("digite a quantidade de dados"))
     soma_das_jogadas = 0 
     for i in range(0,numero_dados):
-        roll = random.randint(1,6)
+        roll = random.randint(1,numero_de_lados)
         soma_das_jogadas += roll
-        print(f"sua jogada foi: {roll}")
+        if roll == 1:
+            print(f"sua jogada foi: {roll} Falha critica")
+        elif roll == 6:
+            print(f"sua jogada foi: {roll} Acerto Critico")
+        else:
+            print(f"sua jogada foi: {roll}")    
+        
     print(f"o total das jogadas foi: {soma_das_jogadas}")
     
 
