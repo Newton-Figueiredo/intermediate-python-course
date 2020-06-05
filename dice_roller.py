4b71d03a6136a33cb14c8fcd44665d5ab063d9ee
newcarvalho <new.figueiredo@gmail.com>
2020-06-04 21:14:40 -0300

    agora o programa tem 2 dados

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,14 @@
 import random
 
 def main():
-    roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
+    numero_dados = 2
+    soma_das_jogadas = 0 
+    for i in range(0,numero_dados):
+        roll = random.randint(1,6)
+        soma_das_jogadas += roll
+        print(f"sua jogada foi: {roll}")
+    print(f"o total das jogadas foi: {soma_das_jogadas}")
+    
 
 if __name__== "__main__":
   main()
